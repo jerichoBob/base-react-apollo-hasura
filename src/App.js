809911aa52ba7c695e6f8c3c158e5353d82d8cd0
Bob Seaton <robert.w.seaton.jr@gmail.com>
2022-06-09 13:40:10 -0400

    moved from ReactMD to MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import './App.scss';
 
-import React, {useState} from 'react';
+import React, {  } from 'react';
 import {
   ApolloClient,
   InMemoryCache, useQuery,
   gql,
   NetworkStatus
 } from "@apollo/client";
-import { Button } from "@react-md/button";
-import { Card, CardHeader, CardTitle,  CardContent, CardActions } from "@react-md/card"
-import { Avatar } from "@react-md/avatar";
-import { Typography } from "@react-md/typography"
+
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import Divider from '@mui/material/Divider';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Paper from '@mui/material/Paper';
+
+import { red } from '@mui/material/colors';
+import Stack from '@mui/material/Stack';
+
+import Typography from '@mui/material/Typography';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export const client = new ApolloClient({
   uri: 'https://exciting-mammal-78.hasura.app/v1/graphql',
@@ -27,15 +55,10 @@ const QUERY_GET_PEOPLE = gql`
       people {index, first_name, last_name}
     }
 `;
-/**
- * 
- * @returns 
- */
-const getClients = () => {
-
-}
 
 const ImportantPeople = () => {
+  // let [clientList, setClientList] = useState([]);
+
   const { loading, error, data, networkStatus } = useQuery(
     QUERY_GET_PEOPLE, {
     notifyOnNetworkStatusChange: true,
@@ -43,22 +66,51 @@ const ImportantPeople = () => {
   if (networkStatus === NetworkStatus.refetch) return 'Refetching!';
   if (loading) return null;
   if (error) return `Error! ${error}`;
+  // setClientList(data);
+
+  return (
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+      divider={<Divider  flexItem />}
+      >
+        {data.people.map((person,index, array) => {
+          return(
+          <div key={index}>
+          <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText primary={`${index} :: ${person.first_name} ${person.last_name}`} />
+          </ListItemButton>
+        </ListItem>
+        {person.index < array.length && <Divider  component="li" />}
+        
+        
+          
+        </div> );         
+      })}
+      </List>
+      
 
 
-  return data.people.map((person) => (
-    <div key={person.index}>
-      <Button>
-        Name[{person.index}]: {person.first_name} {person.last_name}
-      </Button>
-    </div>
-  ));
+  );
 };
 
 const ClientDetails = () => (
-  <Card>
-    <CardHeader beforeChildren={<Avatar>A</Avatar>}>
-      <CardTitle>Client Details</CardTitle>
-    </CardHeader>
+
+  <Card sx={{ width: 345 }}>
+
+    <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="client-details">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
     <CardContent>
       <Typography> Here is some text to display in the card. It is assumed this sentence will end.</Typography>
     </CardContent>
@@ -70,27 +122,43 @@ const ClientDetails = () => (
 );
 
 export const App = () => {
-  let [clientList, setClientList] = useState([]);
 
   return(
-    <Card>
-      <CardHeader>
-        <CardTitle>Application 🚀</CardTitle>
+    <Box sx={{ width: '100%', maxWidth: 700, bgcolor: 'background.paper', border: '1px dashed grey' }}>
+
+    <Stack
+      direction={{ xs: 'column', sm: 'row' }}
+      divider={<Divider orientation="vertical" flexItem />}
+
+      spacing={{ xs: 1, sm: 2, md: 4 }}
+    >
+      <Item>
+      {/* <CardHeader>
+        <Typography variant="body2" color="text.secondary">
+          Application 🚀
+        </Typography>        
       </CardHeader>    
-      <CardContent>
-        <Card>
+
+      <CardContent> */}
+        <Card sx={{ width: 345 }}>
           <CardHeader>
-            <CardTitle>Clients</CardTitle>
+            <Typography component="div" variant="h5">
+              Clients
+            </Typography>                    
           </CardHeader>        
           <CardContent>
             <ImportantPeople />
           </CardContent>
         </Card>
-
-        <ClientDetails/>
-      </CardContent>
-    </Card>
-    );
+        </Item>
+        
+        <Item>
+          <ClientDetails/>
+        </Item>
+      {/* </CardContent> */}
+    </Stack>
+    </Box>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ApolloProvider} from "@apollo/client";
 import { client, App } from './App';
-import { Configuration } from "@react-md/layout";
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
 
 
 const container = document.getElementById('root');
@@ -12,8 +16,10 @@ const root = createRoot(container);
 
 root.render(
   <ApolloProvider client={client}>
-    <Configuration>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
       <App />
-    </Configuration>
+    </ThemeProvider>
   </ApolloProvider>
 );
